37f9c1bb80e6209e53815300b2511b274c22e50b
Norberto Moreno <nmv245@gmail.com>
2021-01-21 17:23:35 -0500

    Add files via upload

---
+++ Dj/posts/apps.py
@@ -0,0 +1,9 @@
+"""Post application module."""
+
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+	""" Post application settings."""
+    name = 'posts'
+    varbose_name = 'Posts'
