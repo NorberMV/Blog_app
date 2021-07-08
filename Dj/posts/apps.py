666398deea643c1ec72ca8f837bcf2e1d6e053cf
Norberto Moreno <nmv245@gmail.com>
2021-07-08 18:13:13 -0500

    Delete Dj directory

--- Dj/posts/apps.py
+++
@@ -1,9 +0,0 @@
-"""Post application module."""
-
-from django.apps import AppConfig
-
-
-class PostsConfig(AppConfig):
-	""" Post application settings."""
-    name = 'posts'
-    varbose_name = 'Posts'
